cd099a7894b1c1170ecdd5b4703e57114f138612
iTomGeller <303327065@qq.com>
2024-09-18 11:53:24 +0800

    最小覆盖子串

--- 子串/最小覆盖子串.cpp
+++ 子串/最小覆盖子串.cpp
@@ -2,37 +2,35 @@
 #include <cstring>
 using namespace std;
 const int N = 128;
-int count[N];
+int counta[N], countb[N];
 int ans = 3e6;
 int startpos = 0;
 string s, t; 
 int check()
 {
-	for(int i = 0; i < t.length(); i++)
-	if(count[int(t[i])] > 0)return 0;
+	for(int i = 0; i < N; i++)
+	if(counta[i] < countb[i])return 0;
 	return 1;
 }
 int main()
 {
 	cin >> s >> t;
 	int n = s.length(), m = t.length();
-	for(int i = 0; i < m; i++)count[int(t[i])]++;
+	for(int i = 0; i < m; i++)countb[int(t[i])]++;
 	if(n < m){cout << "";return 0;}
 	for(int i = 0; i < m; i++)
-	count[int(s[i])]--;
-	cout << count[int('a')] << endl;
+	counta[int(s[i])]++;
 	for(int j = m - 1, i = 0; j < n; j++)
 	{
 		if(j != m - 1)
-		count[int(s[j])]--;
+		counta[int(s[j])]++;
 		while(check() && i <= j)
 		{
 		if(j - i + 1 <= ans)ans = j - i + 1, startpos = i;
-		count[int(s[i])]++;
+		counta[int(s[i])]--;
 		i++;
 		}
 	}
-	cout << ans;
 	if(ans == 3e6)cout << "";
 	else cout << s.substr(startpos, ans);
 }
